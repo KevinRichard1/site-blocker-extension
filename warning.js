472a706e817d616b62194a8a0a453de91c8b4afe
KevinRichard1 <kevin.rohit@gmail.com>
2025-03-07 11:44:23 -0500

    added overlay element

---
+++ warning.js
@@ -0,0 +1,61 @@
+let userUrls = [];
+
+//Disable right click on overlay
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+}, false);
+
+//Get list of saved URLs
+if (typeof browser !== "undefined"){
+    browser.storage.local.get({ userUrls: []})
+        .then((result) => {
+            userUrls = Array.isArray(result.userUrls) ? result.userUrls : [];
+            urlCheck(userUrls);
+        })
+        .catch((error) => {
+            console.error("Error fetching storage data:", error);
+        });
+} else {
+    chrome.storage.local.get({ userUrls: [] }, (result) => {
+        userUrls = Array.isArray(result.userUrls) ? result.userUrls : [];
+        urlCheck(userUrls);
+    });
+}
+
+//Function to check and block url
+function urlCheck(userUrls){
+    //Get current URL
+    const currentTab = "*://" + window.location.hostname + "/*"
+
+    //Check if current URL is in the list
+    if (userUrls.includes(currentTab)){
+        //Create an overlay for the page
+        const overlay = document.createElement('div');
+
+        overlay.style.width = '100%';
+        overlay.style.height = '100%';
+        overlay.style.position = 'fixed';
+        overlay.style.zIndex = '99999';
+        overlay.style.backgroundColor = '#FFFFFF';
+        overlay.style.color = '#000000';
+        overlay.style.display = 'flex';
+        overlay.style.justifyContent = 'center';
+        overlay.style.alignItems = 'center';
+
+        overlay.innerHTML = '<h1 style="margin: auto;">This site is blocked</h1>';
+        
+
+        //Add overlay to page
+        document.body.append(overlay);
+        console.log("Site blocked");
+
+        //Re-add overlay if it has been deleted
+        setInterval(reappend(), 100);
+
+        function reappend(){
+            if(!document.body.contains(overlay)){
+                document.body.append(overlay)
+            }
+        }
+    }
+}
